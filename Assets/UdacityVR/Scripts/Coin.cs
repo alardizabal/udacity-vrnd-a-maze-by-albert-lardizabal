3dc2f696087218408b994adcc8eb2ec49f9f18fb
Albert Lardizabal <albie06@gmail.com>
2017-10-08 15:51:58 -0400

    added coin poof effect

--- Assets/UdacityVR/Scripts/Coin.cs
+++ Assets/UdacityVR/Scripts/Coin.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour 
 {
-    //Create a reference to the CoinPoofPrefab
+    public GameObject coinPoof;
 
     public void OnCoinClicked() {
-        // Instantiate the CoinPoof Prefab where this coin is located
-        // Make sure the poof animates vertically
-        // Destroy this coin. Check the Unity documentation on how to use Destroy
-        GetComponent<AudioSource>().Play();
+		Vector3 coinLocation = gameObject.transform.position;
+		Destroy(gameObject);
+        Instantiate(coinPoof, new Vector3(coinLocation.x, coinLocation.y, coinLocation.z), Quaternion.Euler(-90f, 0, 0));
+        coinPoof.GetComponent<AudioSource>().Play();
     }
-
 }
