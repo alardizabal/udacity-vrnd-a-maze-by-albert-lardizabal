75aa59d9289238107d9d2c62a04e1d81653ded1d
Albert Lardizabal <albie06@gmail.com>
2017-10-09 09:26:12 -0400

    added sign post script to reset scene

--- Assets/UdacityVR/Scripts/Door.cs
+++ Assets/UdacityVR/Scripts/Door.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class Door : MonoBehaviour 
 {
-    private Key key;
+    public Animator doorAnimation;
+	public AudioClip doorLocked;
+	public AudioClip doorOpened;
+    public Key key;
     private bool _isUnlocked = false;
     private bool _isOpening = false;
     // Create a boolean value called "locked" that can be checked in OnDoorClicked() 
     // Create a boolean value called "opening" that can be checked in Update() 
 
+
     void Update() {
         // If the door is opening and it is not fully raised
         // Animate the door raising up
         if (!_isOpening) {
-            
+            doorAnimation.StartPlayback();
         }
     }
 
@@ -25,9 +29,11 @@ public class Door : MonoBehaviour
         // Play a sound to indicate the door is locked
         if (key.isCollected && _isUnlocked) {
             _isOpening = true;
+            gameObject.GetComponent<AudioSource>().clip = doorOpened;
         } else {
-            
+            gameObject.GetComponent<AudioSource>().clip = doorLocked;
         }
+        gameObject.GetComponent<AudioSource>().Play();
     }
 
     public void Unlock()
